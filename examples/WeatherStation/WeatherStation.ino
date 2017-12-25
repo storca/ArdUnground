1b3f5d2959c59e87e6bdfbbddee7e840daec3808
storca <geek392662@gmail.com>
2017-12-25 20:17:50 +0100

    Update WeatherStation.ino

--- examples/WeatherStation/WeatherStation.ino
+++ examples/WeatherStation/WeatherStation.ino
@@ -1,4 +1,6 @@
-#include <ArdUnground.h>
+//Local directory
+#include "ArdUnground.h"
+//Native library
 #include <Ethernet.h>
 
 //Reference for Ethernet library : https://www.arduino.cc/en/Tutorial/WebClient
